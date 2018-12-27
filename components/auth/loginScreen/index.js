3077d36410e8de54b1695574401f72c541c93599
Sharan Khan <sharan.gohar@gmail.com>
2018-12-27 16:44:12 +0500

    added validation in login Screen

--- components/auth/loginScreen/index.js
+++ components/auth/loginScreen/index.js
@@ -1,72 +1,151 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import { View, Image, ImageBackground, TextInput } from 'react-native';
 import { Text, Button, Icon } from 'native-base';
 import styles from './styles'
+import Validate from 'validate.js'
+import { ValidateLogin } from '../../../store/actions/auth/login'
 
-export default class LoginScreen extends React.Component {
+const constraints = {
+  username,
+  password
+};
+const username = {
+  presence: true,
+  email: true
+}
+const password = {
+  presence: true,
+  length: {
+    minimum: 6
+  }
+}
+
+class LoginScreen extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      email: '',
+      username: '',
       password: '',
+      usernameError: '',
+      passwordError: ''
     }
+    this.validateEmail = this.validateEmail.bind(this)
+    this.validatePassword = this.validatePassword.bind(this)
+  }
+  validateEmail(e) {
+    const error = Validate({ username: this.state.username }, { username })
+    if (error) {
+      console.log(error)
+      this.setState({ usernameError: error.username[0] })
+      return false
+    }
+    this.setState({ usernameError: '' })
+    return true
   }
 
-  onPressLearnMore = () => {
-    console.log('hola')
+  validatePassword(e) {
+    const error = Validate({ password: this.state.password }, { password })
+    if (error) {
+      console.log(error)
+      this.setState({ passwordError: error.password[0] })
+      return false
+    }
+    this.setState({ passwordError: '' })
+    return true
   }
 
-  handleChange = (name, val) => {
-    this.setState({
-      [name]: val
-    })
+  validateLogin(username, password) {
+    if (!this.validateEmail()) {
+      this.usernameInput.focus()
+      return
+    }
+    if (!this.validatePassword()) {
+      this.passwordInput.focus()
+      return
+    }
+    this.props.validateLogin(username, password)
   }
-
+  
   render() {
-      return (
-        <ImageBackground style={styles.ImageBackground} source={require('../../../assets/background-login.png')}>
-          <View style={styles.ScreenContainer}>
-            <View style={styles.container}>
-              <View style={styles.logoContainer}>
-                <View style={styles.logoContainerImageBox}>
-                  <Image style={styles.logoContainerImage} source={require('../../../assets/todo_logo.png')} />
-                </View>
-                <View style={styles.logoContainerHeadlineBox}>
-                  <Text style={styles.logoContainerHeadline}>Welcome to Todo App</Text>
-                </View>
+    return (
+      <ImageBackground style={styles.ImageBackground} source={require('../../../assets/background-login.png')}>
+        <View style={styles.ScreenContainer}>
+          <View style={styles.container}>
+            <View style={styles.logoContainer}>
+              <View style={styles.logoContainerImageBox}>
+                <Image style={styles.logoContainerImage} source={require('../../../assets/todo_logo.png')} />
               </View>
-              <View style={styles.formContainer}>
+              <View style={styles.logoContainerHeadlineBox}>
+                <Text style={styles.logoContainerHeadline}>Welcome to Todo App</Text>
+              </View>
+            </View>
+            <View style={styles.formContainer}>
+              <View style={styles.formContainerBoxContainer}>
                 <View style={styles.formContainerUsernameBox}>
-                  <Icon style={styles.formContainerUsernameIcon} type="FontAwesome" name='user'/>
-                  <TextInput style={styles.formContainerUsernameInput} keyboardType='email-address' returnKeyType='next' placeholder="Enter your username/email" />
+                  <Icon style={styles.formContainerUsernameIcon} type="FontAwesome" name='user' />
+                  <TextInput
+                    style={styles.formContainerUsernameInput}
+                    keyboardType='email-address' returnKeyType='next'
+                    placeholder="Enter your username/email"
+                    onBlur={this.validateEmail}
+                    onChangeText={(value) => { this.setState({ username: value.trim() }) }}
+                    value={this.state.username}
+                    ref={input => { this.usernameInput = input }} />
                 </View>
-                <View  style={styles.formContainerPasswordBox}>
+                <Text style={styles.formContainerValidation}>{this.state.usernameError}</Text>
+              </View>
+              <View style={styles.formContainerBoxContainer}>
+                <View style={styles.formContainerPasswordBox}>
                   <Icon style={styles.formContainerPasswordIcon} type="FontAwesome" name='lock' />
-                  <TextInput style={styles.formContainerPasswordInput} placeholder="Enter your password" returnKeyType='done' secureTextEntry={true} />
+                  <TextInput
+                    style={styles.formContainerPasswordInput}
+                    placeholder="Enter your password"
+                    returnKeyType='done'
+                    secureTextEntry={true}
+                    onBlur={this.validatePassword}
+                    onChangeText={(value) => { this.setState({ password: value.trim() }) }}
+                    secureTextEntry={true}
+                    ref={input => { this.passwordInput = input }} />
                 </View>
-                <View style={styles.formContainerLoginBox}>
-                  <Button style={styles.formContainerLoginButton} block success><Text>Login</Text></Button>
-                </View>
-                <View style={styles.orContainer}>
-                  <View style={styles.orContainerLeft}></View>
-                  <View><Text style={styles.orContainerText}>OR</Text></View>
-                  <View style={styles.orContainerRight}></View>
-                </View>
-                <View style={styles.googleAuthContainer}>
-                  <Button style={styles.googleAuthContainerButton} block danger>
-                    <Image style={styles.googleAuthContainerImage} source={require('../../../assets/googleAuthImage.png')} />
-                    <Text>Continue with Google</Text>
-                  </Button>
-                </View>
-                <View style={styles.formFooterContainer}>
-                  <Text style={styles.formFooterForgot}>Forgot password?</Text>
-                  <Text style={styles.formFooterSignUp}>New here? Sign Up</Text>
-                </View>
+                <Text style={styles.formContainerValidation}>{this.state.passwordError}</Text>
+              </View>
+              <View style={styles.formContainerLoginBox}>
+                <Button
+                  style={styles.formContainerLoginButton}
+                  block
+                  success
+                  onPress={() => { this.validateLogin(this.state.username, this.state.password) }}><Text>Login</Text></Button>
+              </View>
+              <View style={styles.orContainer}>
+                <View style={styles.orContainerLeft}></View>
+                <View><Text style={styles.orContainerText}>OR</Text></View>
+                <View style={styles.orContainerRight}></View>
+              </View>
+              <View style={styles.googleAuthContainer}>
+                <Button style={styles.googleAuthContainerButton} block danger>
+                  <Image style={styles.googleAuthContainerImage} source={require('../../../assets/googleAuthImage.png')} />
+                  <Text>Continue with Google</Text>
+                </Button>
+              </View>
+              <View style={styles.formFooterContainer}>
+                <Text style={styles.formFooterForgot}>Forgot password?</Text>
+                <Text style={styles.formFooterSignUp}>New here? Sign Up</Text>
               </View>
             </View>
           </View>
-        </ImageBackground>
-      );
+        </View>
+      </ImageBackground>
+    );
   }
 }
+const mapStateToProps = (state) => ({
+  user: state.loginReducer.user
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  validateLogin: (username, password) => dispatch(ValidateLogin(username, password))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen)
--- components/auth/loginScreen/styles.js
+++ components/auth/loginScreen/styles.js
@@ -38,6 +38,13 @@ export default {
       paddingTop: 50, 
       justifyContent: 'space-between' 
     },
+    formContainerValidation: {
+      color:'red'
+    },
+    formContainerBoxContainer: {
+      flexDirection:'column'
+    },
+
     formContainerUsernameBox: {
       width: '100%', 
       flexDirection: 'row', 
