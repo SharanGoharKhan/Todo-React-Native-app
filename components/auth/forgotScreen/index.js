a4e25e643b1340361471160f73ebe54ad22a6f7d
Sharan Khan <sharan.gohar@gmail.com>
2019-01-09 17:03:14 +0500

    added forgot screen

--- components/auth/forgotScreen/index.js
+++ components/auth/forgotScreen/index.js
@@ -1,32 +1,69 @@
 import React from 'react'
-import { View, Text, TextInput } from 'react-native'
-import { Button } from 'native-base'
+import { View, Text, TextInput, ImageBackground } from 'react-native'
+import { Button, Icon } from 'native-base'
 import styles from "./styles";
+import Validate from 'validate.js'
 
 class ForgotScreen extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: ''
+        }
+        this.validateEmail = this.validateEmail.bind(this)
+    }
+    validateEmail(e) {
+        const error = Validate({ username: this.state.username }, { username })
+        if (error) {
+            console.log(error)
+            this.setState({ usernameError: error.username[0] })
+            return false
+        }
+        this.setState({ usernameError: '' })
+        return true
+    }
+    onSendEmail() {
+        alert('email sent')
+    }
+
     render() {
         return (
-            <View style={{flex: 1,flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <View>
-                    <Text>Forgot your password?</Text>
+            <ImageBackground style={styles.ImageBackground} source={require('../../../assets/background-login.png')}>
+                <View style={{flex:1,justifyContent:'center',alignItems:'center' }}>
+                    <View style={{ width: '80%'}}>
+                        <View style={{alignItems:'center',marginBottom:20}}>
+                            <Text style={{ fontSize: 20, color: 'white' }}>Forgot your password?</Text>
+                        </View>
+                        <View style={styles.formContainerBoxContainer}>
+                            <View style={styles.formContainerUsernameBox}>
+                                <Icon style={styles.formContainerUsernameIcon} type="AntDesign" name='mail' />
+                                <TextInput
+                                    style={styles.formContainerUsernameInput}
+                                    keyboardType='email-address' returnKeyType='next'
+                                    placeholder="email"
+                                    onBlur={this.validateEmail}
+                                    onChangeText={(value) => { this.setState({ username: value.trim() }) }}
+                                    value={this.state.username}
+                                    ref={input => { this.usernameInput = input }} />
+                            </View>
+                            <Text style={styles.formContainerValidation}>{this.state.usernameError}</Text>
+                        </View>
+                        <View style={styles.formContainerLoginBox}>
+                            <Button
+                                style={styles.formContainerLoginButton}
+                                block
+                                success
+                                onPress={() => { this.onSendEmail() }}>
+                                <Text style={{ color: 'white' }}>Send Email</Text></Button>
+                        </View>
+                    </View>
                 </View>
-                <View>
-                    <TextInput
-                        style={styles.formContainerUsernameInput}
-                        keyboardType='email-address' returnKeyType='next'
-                        placeholder="Email"
-                    />
-                </View>
-                <View>
-                    <Button
-                        block
-                        success>
-                        <Text>Send Email</Text>
-                    </Button>
-                </View>
-            </View>
+            </ImageBackground>
         )
     }
 }
+
+
 
 export default ForgotScreen
--- components/auth/forgotScreen/styles.js
+++ components/auth/forgotScreen/styles.js
@@ -2,5 +2,48 @@ export default {
     formContainerUsernameInput: {
         width: '100%',
         fontSize: 18
+    },
+    ImageBackground: {
+        width: '100%',
+        height: '100%'
+    },
+    formContainerBoxContainer: {
+        flexDirection: 'column',
+        marginBottom: 20
+    },
+    formContainerValidation: {
+        color: 'red'
+    },
+    formContainerUsernameBox: {
+        width: '100%',
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        marginBottom: 10,
+        justifyContent: 'flex-start',
+        padding: 10
+    },
+    formContainerUsernameIcon: {
+        width: 30,
+        height: 30,
+        marginRight: 10
+    },
+    formContainerUsernameInput: {
+        width: '100%',
+        fontSize: 18
+    },
+    formContainerLoginBox: {
+        width: '100%', 
+        marginBottom: 10, 
+        flexDirection: 'row'
+      },
+      formContainerLoginButton: {
+        width: '100%'
+      },
+      formContainerLoginButtonDisabled: {
+        width: '100%',
+        backgroundColor: 'gray'
       }
+
 }
