9f4451611152ebbd88d31a8af4179048a2208e3a
Asad Jakhar <rustyonrampage@hotmail.com>
2019-01-30 15:56:44 +0500

    todo modal

--- components/screens/todos/index.js
+++ components/screens/todos/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Keyboard} from 'react-native'
+import {Keyboard, TouchableOpacity, Modal} from 'react-native'
+import { Dialog } from "react-native-simple-dialogs";
+
 import {
     View,
     Container,
@@ -17,6 +19,7 @@ import {
 } from "native-base";
 import TextInput from 'native-base'
 
+
 import HeaderView from '../../../ui/header'
 import styles from "./styles";
 
@@ -26,15 +29,14 @@ class Todos extends React.Component {
 
 
         this.state = {
-            input_name:"",
-            input_desc:"",
-            list_todos:[
-                {name:"Go to uni", desc:"Sir Waseem 1visit karna hai"},
-                {name:"Go to uni2", desc:"Sir Waseem v2isit karna hai"},
-                {name:"Go to uni3", desc:"Sir Waseem v2isit karna hai"},
-                
-
-  
+            input_name: "",
+            input_desc: "",
+            modalVisible: false,
+            active_todo_id: 0,
+            list_todos: [
+                { name:"Go to uni", desc:"Sir Waseem 1visit karna hai"},
+                { name:"Go to uni2", desc:"Sir Waseem v2isit karna hai"},
+                { name:"Go to uni3", desc:"Sir Waseem v2isit karna hai"},
             ]
         }
     }
@@ -77,6 +79,17 @@ class Todos extends React.Component {
         })
     }
 
+    setModalVisible=(visible,id) =>{
+        if(id!==undefined){
+            console.log( this.state.list_todos[id] )
+            console.log(id)
+            this.setState({modalVisible: visible, active_todo_id: id});
+        }else
+        this.setState({modalVisible: visible})
+        
+      }
+    
+
     render() {
         return (
             <Container style={styles.container}>
@@ -94,15 +107,16 @@ class Todos extends React.Component {
                                 <Input style={{width:"100%",marginLeft:0}} value={this.state.input_name} placeholder='Name' onChangeText={this.HandleNameChg} />
                             </Item>
                             <Textarea value={this.state.input_desc} onChangeText={this.HandleDescChg}  style={ styles.input_desc} rowSpan={5} bordered placeholder="Description" />
-                            <Button onPress={this.AddTodos} style={{ width:"100%"}}   primary><Text> Submit </Text></Button>
+                            <Button full onPress={this.AddTodos} style={{  flex:1, flexGrow:1,flexDirection:"row", textAlign: 'center', height: 30 }}   primary><Text> Submit </Text></Button>
 
                             </View>
                         </Tab>
                         <Tab heading="List TODOs">
                             <View style={styles.todo_container}>
-                                {   this.state.list_todos.map( (todo)=>(
-                               
-                                    <Card  style={{width: "30%", height:180,marginRight: 'auto'}}>
+                                {   this.state.list_todos.map( (todo, i)=>(
+                                   
+                                  <TouchableOpacity key={ i } activeOpacity={0.6} style={{width: "30%", height:180,marginRight: 'auto', marginBottom:10}}  onPress={()=>{this.setModalVisible(true, i)}}>
+                                    <Card key={ i } style={{width:"100%", height:"100%", padding:0, margin:0}}>
                                         <CardItem header>
                                         <Text>{todo.name}</Text>
                                         </CardItem>
@@ -111,12 +125,13 @@ class Todos extends React.Component {
                                             <Text>
                                                 {
                                                     todo.desc.length > 15? todo.desc.substr(0,15)+"...": todo.desc
-                                                    }
+                                                }
                                             </Text>
                                         </Body>
                                         </CardItem>
-                                
                                     </Card>
+                                  </TouchableOpacity>
+
                                     )
                                 )}
         
@@ -124,7 +139,39 @@ class Todos extends React.Component {
                         </Tab>
                      
                     </Tabs>
+                    
+                    <View style={{fontSize: 16}} >
+
+                    <Dialog
+                        animationType="fade"
+                        contentStyle={{margin:0, padding:0}}
+                        style={{marginTop:0, paddingTop:0}}
+                        onTouchOutside={ () => this.setModalVisible(false) }
+                        visible={ this.state.modalVisible }
+                    >
+                        <Text full style={{margin:0, padding:0,fontSize: 18, paddingLeft:0, backgroundColor:"#388e3c", 
+                        fontWeight:"bold", color:"#fff"}}> Edit TODO </Text>
+                        <Text>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </Text>
+                        <Text >
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </Text>
+                 
+              
+                    <Button
+                        onPress={ () => this.setModalVisible(false) }
+                        style={ { marginTop: 10 } }
+                        title="CLOSE"
+                    />
+                </Dialog>
+
+                      
+
+                      
                     </View>
+                </View>
 
 
             
