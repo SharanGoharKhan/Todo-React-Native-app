d30a0ef1b8947971d345f8e56c5351510b5343a5
Sharan Khan <sharan.gohar@gmail.com>
2019-01-30 16:21:27 +0500

    home changes

--- components/screens/home/index.js
+++ components/screens/home/index.js
@@ -4,18 +4,26 @@ import {
     Content,
     Text,
 } from "native-base";
+import {
+    View,
+    Dimensions
+} from 'react-native'
 import HeaderView from '../../../ui/header'
 import styles from "./styles";
 
 class Home extends React.Component {
     render() {
+        const deviceWidth = Dimensions.get('window').width
+        const deviceHeight = Dimensions.get('window').height
         return (
             <Container style={styles.container}>
                 <HeaderView 
                 title='Home'
                 navigationObj={this.props.navigation}/>
                 <Content padder>
-                    <Text>Hello from Home</Text>
+                    <View style={{alignItems:'center'}}>
+                        <Text style={{fontSize:20,color: '#03dac6',fontWeight:'700'}}>Welcome to Todo Starter kit</Text>
+                    </View>
                 </Content>
             </Container>
         )
